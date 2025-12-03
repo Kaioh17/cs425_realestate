c38902ba61b45a5c53b7a3f88495cfd74fd655eb
mubaraq <mubskill@gmail.com>
2025-12-03 14:01:40 -0600

    booking update

--- app/operations/booking.py
+++ app/operations/booking.py
@@ -10,3 +10,24 @@ Responsibilities:
 # Implementation notes:
 # - Integrate with Payment and Property models; record payment method and status.
 # - Enforce availability checks before creating a booking.
+from app.db.connect import get_db
+from sqlalchemy import text
+from . import helper_service
+from app.db.schemas import UserCreate, AgentCreate
+from .helper_service import query_
+class Agent:
+    db_gen = get_db()
+    db = next(db_gen)
+    
+    def get_all_bookings():
+        try:
+            sql = "select * from bookings"
+            
+            result = query_(db=Agent.db).select_all(query=sql)
+            
+            return result
+        except Exception as e:
+            raise e
+    
+if __name__ == '__main__':
+    Agent.get_all_bookings()
--- app/operations/helper_service.py
+++ app/operations/helper_service.py
@@ -5,23 +5,65 @@ from sqlalchemy import text
 class query_:
     def __init__(self, db):
         self.db = db
-    def select_all(self, user ,query: str = None):
+    def select_all(self,query: str = None, param: str = None):
         """This helper is used for select queries and execution:
                 - it prevents repetitive coding
                 - and accepts basic sql query as strings 
             """
+        _Validators._ensure_query(query)
         stmt = text(query)
-        result = self.db.execute(stmt).fetchall()
-        user._print_all(result)
-    
+        
+        if not param:
+            result = self.db.execute(stmt)
+        elif param:
+            result = self.db.execute(stmt, param)
+            
+        rows = result.mappings().all()
+        # self._print_all(result)
+        print(rows)
+        return rows
     def _insert(self, query, params: dict):
+        
+        _Validators._ensure_params(params)
+        _Validators._ensure_query(query)
         sql_query = text(query)
         result = self.db.execute(sql_query, params)
        
         self.db.commit()
         
         return result
+    def _delete_by(self, query: str=None, 
+                   param: any=None):
+        """
+            _delete_by: delete by params
+
+        Args:
+            query (str, optional): _description_. Defaults to None.
+            param (any, optional): _description_. Defaults to None.
+
+        Raises:
+            ValueError: _description_
+        """
+        try:
+            _Validators._ensure_params(param)
+            _Validators._ensure_query(query)
+            sql_query = text(query)
+            
+            response = self.db.execute(sql_query, param)
+            
+            return 
+        
+        except Exception as e:
+            raise e
     def _print_all(self, result):
         for i in result:
             print(i, "\n")
-        
+             
+class _Validators:
+    
+    def _ensure_query(query):
+        if not query:
+            raise ValueError('Query parameter needs to be filled')
+    def _ensure_params(param):
+        if not param: 
+            raise ValueError('Param is needed to filter search')
